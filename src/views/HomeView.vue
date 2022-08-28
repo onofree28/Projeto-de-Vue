881974abbae8ce4abd0394cae510e228117a11c6
onofree28 <onofre_luiz@hotmail.com>
2022-08-28 11:27:26 -0300

    Desenvolvendo o Projeto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="home">
-    <h1>HOME</h1>
+  <div class="main-container">
+    <Banner/>
+    <h1>Monte o seu Burger</h1>
+    <p>formulário</p>
   </div>
 </template>
 
 <script>
+import Banner from "@/components/Banner.vue";
+
+export default {
+  name: 'Home',
+  components: {
+    Banner
+  }
+}
 
 </script>
